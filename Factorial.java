2547edcb26fd0ca0641a129276ca6d85fdc280ae
VR Yeshwanth <124450398+VRYeshwanth@users.noreply.github.com>
2024-07-22 18:14:19 +0530

    Implemented Recursion

--- Factorial.java
+++ Factorial.java
@@ -4,10 +4,9 @@ public class Factorial
 {
     public static long findFactorial(int num)
     {
-        int prod = 1;
-        for(int i = 1; i <= num; i++)
-            prod *= i;
-        return prod;
+        if (num == 1 || num == 0)
+            return 1;
+        return num * findFactorial(num-1);
     }
     public static void main(String args[])
     {
--- GCD.java
+++ GCD.java
@@ -2,23 +2,27 @@ import java.util.Scanner;
 
 public class GCD
 {
+    public static void GCD_Recursion(int max, int min)
+    {
+        int rem;
+        rem = max % min;
+        if(rem == 0)
+        {
+            System.out.println(min);
+            return;
+        }
+        GCD_Recursion(min, rem);
+    }
+
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two numbers: ");
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
-        int rem = 2;
         int dividend = Math.max(n1, n2);
         int divisor = Math.min(n1, n2);
-        while(rem != 0)
-        {
-            rem = dividend % divisor;
-            dividend = divisor;
-            if (rem != 0)
-                divisor = rem;
-        }
-        System.out.println("The GCD of " + n1 + " and " + n2 + " is: " + dividend);
+        GCD_Recursion(dividend, divisor);
         sc.close();
     }
 }
