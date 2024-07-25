d3c428963e37746c8707f2d9477d99476b29c12c
VR Yeshwanth <124450398+VRYeshwanth@users.noreply.github.com>
2024-07-25 13:44:13 +0530

    Implemented recursion

--- Palindrome.java
+++ Palindrome.java
@@ -2,17 +2,34 @@ import java.util.Scanner;
 
 public class Palindrome extends StringReverse
 {
+    //This code checks for palindromes by checking the first and last characters of the string. After that , it takes a substring of the string which does not contain the initial first and last characters and checks them again untill only one character is left, at which point it returns true
+    public static boolean isPalindrome(String str)
+    {
+        if(str.length() <= 1)
+            return true;
+        
+        if(str.charAt(0) != str.charAt(str.length()-1))
+            return false;
+
+        return isPalindrome(str.substring(1,str.length()-1));
+    }
+
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word : ");
         String word = sc.nextLine();
         sc.close();
-        String rev = reverseString(word);
-
-        if (word.equals(rev))
-            System.out.println("It is a palindrome");
+        boolean ans = isPalindrome(word);
+        if(ans)
+            System.out.println(word+ " is a palindrome");
         else
-            System.out.println("It is not a palindrome");
+            System.out.println(word+ " is a palindrome");
+        // String rev = reverseString(word);
+
+        // if (word.equals(rev))
+        //     System.out.println("It is a palindrome");
+        // else
+        //     System.out.println("It is not a palindrome");
     }
 }
--- StringReverse.java
+++ StringReverse.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 
 public class StringReverse
 {
+    public static String reverse(String str)
+    {
+        if(str.length() == 0)
+            return "";
+
+        String end_char = str.substring(str.length()-1);
+        String rem_part = str.substring(0,str.length()-1);
+
+        return end_char + reverse(rem_part);
+    }
+    
     public static String reverseString(String str)
     {
         String rev = "";
@@ -19,7 +30,7 @@ public class StringReverse
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word : ");
         String word = sc.nextLine();
-        String reverse = reverseString(word);
+        String reverse = reverse(word);
         System.out.println("Reverse = "+reverse);
         sc.close();
     }
